b80c895967abc5537266a30477d52a2bdfabe43c
Mario E. Bermonti-Pérez <mbermonti1132@gmail.com>
2023-08-12 21:22:48 -0400

    [example] Make digit sets bigger for testing

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -40,7 +40,7 @@ class _MyHomePageState extends State<MyHomePage> {
           children: <Widget>[
             ElevatedButton(
               onPressed: () {
-                List<String> stimList = ['123', '456'];
+                List<String> stimList = ['0123456789', '9876543210'];
                 Get.put(MDigits(stimList: stimList));
               },
               child: Text(
