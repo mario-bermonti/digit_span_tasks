a3b81cfcacfc0878d233a99717cd783ba1e5a75d
Mario E. Bermonti-Pérez <mbermonti1132@gmail.com>
2023-08-20 19:32:36 -0400

    [refactor] Remove unused modif to stim builder

--- stimuli/lib/builders.dart
+++ stimuli/lib/builders.dart
@@ -1,18 +1,16 @@
 /// Functions that help build a Stimuli object from a file listing the stimuli
 
 import 'dart:io';
-import 'package:flutter/services.dart';
 import 'package:stimuli/errors.dart';
 import 'package:stimuli/stim.dart';
 
 /// Build [Stimuli] from a text file specified by
 /// the path filePath.
 Future<Stimuli> createStimFromFile(String filePath) async {
-  String file = await rootBundle.loadString(filePath);
-  // File file = File(filePath);
-  // await validateStimFileExists(file, filePath);
-  // List<String> stimList = await file.readAsLines();
-  Stimuli stim = Stimuli(stimuli: ['1', '2']);
+  File file = File(filePath);
+  await validateStimFileExists(file, filePath);
+  List<String> stimList = await file.readAsLines();
+  Stimuli stim = Stimuli(stimuli: stimList);
 
   return stim;
 }
