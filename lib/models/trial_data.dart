ca2fa94f8bed2b5adfeb2bda33225f79f7853937
Mario E. Bermonti-Pérez <mbermonti1132@gmail.com>
2023-08-24 11:28:47 -0400

    Add TrialData model

---
+++ lib/models/trial_data.dart
@@ -0,0 +1,10 @@
+class TrialData {
+  final String participantID;
+  final String stim;
+  final String response;
+
+  TrialData(
+      {required this.stim,
+      required this.response,
+      required this.participantID});
+}
