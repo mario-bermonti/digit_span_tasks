808877792a9b4aae603decc25b4b57296ca3dc35
Mario E. Bermonti-Pérez <mbermonti1132@gmail.com>
2023-09-02 15:51:07 -0400

    [docs] Comment functions that randomize sets

--- lib/randomize.dart
+++ lib/randomize.dart
@@ -1,3 +1,6 @@
+/// Returns a randomized list of sets of digits
+/// [stim] list of sets to randomize
+/// Returns a list of the same length as the original
 List<String> randomizeDigitsInSets(List<String> stim) {
   List<String> stimRandomized = <String>[];
   for (String set in stim) {
@@ -7,6 +10,9 @@ List<String> randomizeDigitsInSets(List<String> stim) {
   return stimRandomized;
 }
 
+/// Returns a randomized single digit sets
+/// [set] Digit set to randomize
+/// Returns a String of the same length as the original [set]
 String randomizeSet(String set) {
   List<String> setList = set.split('');
   setList.shuffle();
