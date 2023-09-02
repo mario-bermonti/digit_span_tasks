e84dceb351ba2ca37f1e8f5c7fc6e85f90ba9c7f
Mario E. Bermonti-Pérez <mbermonti1132@gmail.com>
2023-09-02 15:47:46 -0400

    [maint] Remove debuggin print funtions

--- lib/randomize.dart
+++ lib/randomize.dart
@@ -1,10 +1,8 @@
 List<String> randomizeDigitsInSets(List<String> stim) {
   List<String> stimRandomized = <String>[];
   for (String set in stim) {
-    print(set);
     String setString = randomizeSet(set);
     stimRandomized.add(setString);
-    print(setString);
   }
   return stimRandomized;
 }
