e8d8c864738fbcc079757c7fa092be0d970da945
Mario E. Bermonti-Pérez <mbermonti1132@gmail.com>
2023-09-02 15:34:46 -0400

    [example] Improve text in example app

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -48,7 +48,7 @@ class _MyHomePageState extends State<MyHomePage> {
                 ));
               },
               child: Text(
-                'Comenzar mDigits',
+                'mDigits',
                 style: Theme.of(context).textTheme.titleLarge,
               ),
             ),
