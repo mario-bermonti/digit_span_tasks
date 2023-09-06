29c33d631308b902949f925edcb440627d401ac5
Mario E. Bermonti-Pérez <mbermonti1132@gmail.com>
2023-09-06 08:33:56 -0400

    [refactor] Make mdigits_view to stateless

--- lib/mdigits_view.dart
+++ lib/mdigits_view.dart
@@ -2,14 +2,9 @@ import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:mdigits/mdigits.dart';
 
-class MDigitsView extends StatefulWidget {
-  const MDigitsView({super.key});
+class MDigitsView extends StatelessWidget {
+  MDigitsView({super.key});
 
-  @override
-  State<MDigitsView> createState() => _MDigitsViewState();
-}
-
-class _MDigitsViewState extends State<MDigitsView> {
   final MDigits mDigits = Get.put(MDigits());
   @override
   Widget build(BuildContext context) {
