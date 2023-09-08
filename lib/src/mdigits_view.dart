1609cdf8ab27403e3740391f79ace38738de6d93
Mario E. Bermonti-Pérez <mbermonti1132@gmail.com>
2023-09-07 20:46:46 -0400

    [improvement] Make mdigits_view a generic widget
    This new widget will control the flow of execution based on
    mdigits_controller.

--- lib/src/mdigits_view.dart
+++ lib/src/mdigits_view.dart
@@ -1,32 +1,37 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:mdigits/src/end/end_view.dart';
 import 'package:mdigits/src/mdigits_controller.dart';
+import 'package:mdigits/src/response/response_view.dart';
+import 'package:mdigits/src/rest/rest_view.dart';
+import 'package:mdigits/src/stim/stim_view.dart';
+import 'package:mdigits/src/task_step.dart';
 
+/// Controls the task sequence based on the curren step
 class MDigitsView extends StatelessWidget {
   MDigitsView({super.key});
 
   final MDigitsController mDigits =
       Get.put(MDigitsController(), permanent: true);
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            ElevatedButton(
-              onPressed: () async {
-                await mDigits.setup();
-                mDigits.run();
-              },
-              child: Text(
-                'Comenzar',
-                style: Theme.of(context).textTheme.titleLarge,
-              ),
-            ),
-          ],
-        ),
-      ),
-    );
+    return Obx(() {
+      switch (mDigits.status) {
+        case TaskStep.stim:
+          return StimView();
+        case TaskStep.response:
+          return ResponseView();
+        case TaskStep.rest:
+          return RestView();
+        case TaskStep.completed:
+          // TODO should data be processed here
+          if (mDigits.processData != null) {
+            mDigits.processData!(mDigits.data);
+          }
+          return const EndView();
+        // Get.back();
+      }
+    });
   }
 }
