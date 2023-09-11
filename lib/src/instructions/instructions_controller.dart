8a3833f98ee058014de0c1199578886293d8c09d
Mario E. Bermonti-Pérez <mbermonti1132@gmail.com>
2023-09-11 09:22:10 -0400

    [improv] Navigate from instructions to stim screen

--- lib/src/instructions/instructions_controller.dart
+++ lib/src/instructions/instructions_controller.dart
@@ -1,10 +1,14 @@
 import 'package:get/get.dart';
+import 'package:mdigits/src/mdigits_controller.dart';
+import 'package:mdigits/src/task_step.dart';
 
 class InstructionsController extends GetxController {
+  MDigitsController mDigitsController = Get.find();
+
   /// Goes to next screen after 500ms ISI
   void toNextScreen() {
     Future.delayed(const Duration(milliseconds: 500), () {
-      Get.back();
+      mDigitsController.status(TaskStep.stim);
     });
   }
 }
