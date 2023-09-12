5886eef9b87ac379a2e21cf4a79f88c05f64e245
Mario E. Bermonti-Pérez <mbermonti1132@gmail.com>
2023-09-12 08:50:08 -0400

    [refactor] Present stim after rest

--- lib/src/rest/rest_controller.dart
+++ lib/src/rest/rest_controller.dart
@@ -1,10 +1,13 @@
 import 'package:get/get.dart';
+import 'package:mdigits/src/mdigits_controller.dart';
+import 'package:mdigits/src/task_step.dart';
 
 class RestController extends GetxController {
   /// Goes to next screen after 500ms ISI
   void toNextScreen() {
+    MDigitsController mDigitsController = Get.find();
     Future.delayed(const Duration(milliseconds: 500), () {
-      Get.back();
+      mDigitsController.status(TaskStep.stim);
     });
   }
 }
