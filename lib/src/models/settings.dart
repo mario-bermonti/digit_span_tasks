831f724ea3bbc828aef9d4d6b4da910c84e32169
Mario E. Bermonti-Pérez <mbermonti1132@gmail.com>
2023-09-22 08:32:02 -0400

    [maint] Add doc comments and improve formatting

--- lib/src/models/settings.dart
+++ lib/src/models/settings.dart
@@ -1,9 +1,13 @@
 import 'package:mdigits/src/models/trial_data.dart';
 import 'package:mdigits/src/randomize.dart';
+
 class Settings {
   /// List of stim to present to the user
   late final List<String> stimList;
+
+  /// Unique id to identify all the participant's data
   final String participantID;
+
   final void Function(List<TrialData> data)? processData;
 
   Settings({required stimList, required this.participantID, this.processData}) {
