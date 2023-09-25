8d35f3c137c017eda89371566f0d0976d457e2e8
Mario E. Bermonti-Pérez <mbermonti1132@gmail.com>
2023-09-24 21:33:58 -0400

    [improv] Define object to provide data to the user
    [maint] Rename file

---
+++ lib/src/models/mdigits_data.dart
@@ -0,0 +1,16 @@
+import 'package:mdigits/src/models/trial_data.dart';
+
+import 'package:flutter/material.dart';
+
+/// Structured data collected by MDigits
+/// Used to provide data nicely formatted to the user
+class MDigitsData {
+  final List<TrialData> trialData;
+  final TimeOfDay startTime;
+  final TimeOfDay endTime;
+
+  MDigitsData(
+      {required this.trialData,
+      required this.startTime,
+      required this.endTime});
+}
