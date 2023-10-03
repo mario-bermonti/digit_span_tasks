0dd99d3f27bcbb07920e95581ef5cac3d4999eb8
Mario E. Bermonti-Pérez <mbermonti1132@gmail.com>
2023-10-03 10:33:36 -0400

    [maint] Remove dead code from default_appbar

--- lib/src/common/default_appbar.dart
+++ lib/src/common/default_appbar.dart
@@ -7,17 +7,5 @@ createAppBar({required BuildContext context, bool showActionButtons = false}) {
     title: const Text(appBarTitle),
     automaticallyImplyLeading: false,
     centerTitle: true,
-    // actions: showActionButtons ? createActionButtons(context: context) : null,
   );
 }
-
-// List<Widget> createActionButtons({required BuildContext context}) {
-//   List<Widget> actionButtons = <Widget>[
-//     IconButton(
-//       icon: const Icon(Icons.home),
-//       tooltip: 'Inicio',
-//       onPressed: (() => Get.to(MyHomePage())),
-//     ),
-//   ];
-//   return actionButtons;
-// }
