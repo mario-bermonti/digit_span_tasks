57ae7e9c016d84dd8a5727c1c0e2a4c7156dd780
Mario E. Bermonti-Pérez <mbermonti1132@gmail.com>
2023-10-07 10:38:51 -0400

    [improv] Define experimental trials view

---
+++ lib/src/mdigits/mdigits_view_experimental.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:mdigits/src/end/end_view.dart';
+import 'package:mdigits/src/instructions/instructions_view.dart';
+import 'package:mdigits/src/mdigits/mdigits_controller.dart';
+import 'package:mdigits/src/models/settings.dart';
+import 'package:mdigits/src/response/response_view.dart';
+import 'package:mdigits/src/rest/rest_view.dart';
+import 'package:mdigits/src/stim/stim_view.dart';
+import 'package:mdigits/src/mdigits/task_step.dart';
+
+/// Present the appropriate screen based on the curren step
+class MDigitsViewExperimental extends StatelessWidget {
+  final Config config;
+  late final MDigitsController mDigits;
+
+  MDigitsViewExperimental({super.key, required this.config}) {
+    mDigits = Get.put(MDigitsController(config));
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Obx(() {
+      switch (mDigits.taskStep.value) {
+        case TaskStep.instructions:
+          return InstructionsView();
+        case TaskStep.stim:
+          return StimView();
+        case TaskStep.response:
+          return ResponseView();
+        case TaskStep.rest:
+          return RestView();
+        case TaskStep.completed:
+          return EndView();
+      }
+    });
+  }
+}
