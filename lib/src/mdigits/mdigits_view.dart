1ed456d8b3ce71391572a825e57707f24d8452eb
Mario E. Bermonti-Pérez <mbermonti1132@gmail.com>
2023-10-09 09:19:04 -0400

    [improv] Define the MDigits top level widget

--- lib/src/mdigits/mdigits_view.dart
+++ lib/src/mdigits/mdigits_view.dart
@@ -1,38 +1,45 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
-import 'package:mdigits/src/end/end_view.dart';
-import 'package:mdigits/src/instructions/instructions_view.dart';
-import 'package:mdigits/src/mdigits/mdigits_controller.dart';
+import 'package:mdigits/src/mdigits/mdigits_view_experimental.dart';
+import 'package:mdigits/src/mdigits/mdigits_view_practice.dart';
 import 'package:mdigits/src/models/settings.dart';
-import 'package:mdigits/src/response/response_view.dart';
-import 'package:mdigits/src/rest/rest_view.dart';
-import 'package:mdigits/src/stim/stim_view.dart';
-import 'package:mdigits/src/mdigits/task_step.dart';
 
 /// Present the appropriate screen based on the curren step
 class MDigitsView extends StatelessWidget {
   final Config config;
-  late final MDigitsController mDigits;
 
-  MDigitsView({super.key, required this.config}) {
-    mDigits = Get.put(MDigitsController(config));
-  }
+  const MDigitsView({super.key, required this.config});
 
   @override
   Widget build(BuildContext context) {
-    return Obx(() {
-      switch (mDigits.taskStep.value) {
-        case TaskStep.instructions:
-          return InstructionsView();
-        case TaskStep.stim:
-          return StimView();
-        case TaskStep.response:
-          return ResponseView();
-        case TaskStep.rest:
-          return RestView();
-        case TaskStep.completed:
-          return EndView();
-      }
-    });
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            ElevatedButton(
+              onPressed: () async {
+                print('practice');
+                await Get.to(
+                  () => MDigitsViewPractice(
+                    config: config,
+                  ),
+                );
+                print('exp');
+                await Get.to(
+                  () => MDigitsViewExperimental(
+                    config: config,
+                  ),
+                );
+              },
+              child: Text(
+                'General instructions',
+                style: Theme.of(context).textTheme.titleLarge,
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
