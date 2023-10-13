43acf1ecb924fbf417b07bb14fb2ec1df16a33fb
Mario E. Bermonti-Pérez <mbermonti1132@gmail.com>
2023-10-13 09:10:19 -0400

    refactor: Find MDigitController in object field

--- lib/src/end/end_controller.dart
+++ lib/src/end/end_controller.dart
@@ -3,10 +3,10 @@ import 'package:mdigits/src/mdigits/mdigits_controller.dart';
 
 class EndController extends GetxController {
   final Duration waitTime = const Duration(seconds: 2);
+  MDigitsController mDigitsController = Get.find();
 
   /// Ends the session after 2s and returns to MDigits caller
   Future<void> toNextScreen() async {
-    MDigitsController mDigitsController = Get.find();
     await Future.delayed(
       waitTime,
       () => mDigitsController.endSession(),
