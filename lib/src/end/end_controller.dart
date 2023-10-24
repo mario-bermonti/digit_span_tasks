58e9225b65765abb0e5d8b36c9af5def3c830b57
Mario E. Bermonti-Pérez <mbermonti1132@gmail.com>
2023-10-24 10:48:58 -0400

    refactor(end_controller): Make fields private

--- lib/src/end/end_controller.dart
+++ lib/src/end/end_controller.dart
@@ -2,14 +2,14 @@ import 'package:get/get.dart';
 import 'package:mdigits/src/mdigits/mdigits_controller.dart';
 
 class EndController extends GetxController {
-  final Duration waitTime = const Duration(seconds: 2);
-  MDigitsController mDigitsController = Get.find();
+  final Duration _waitTime = const Duration(seconds: 2);
+  final MDigitsController _mDigitsController = Get.find();
 
   /// Ends the session after 2s and returns to MDigits caller
   void toNextScreen() {
     Future.delayed(
-      waitTime,
-      () => mDigitsController.endSession(),
+      _waitTime,
+      () => _mDigitsController.endSession(),
     );
   }
 }
