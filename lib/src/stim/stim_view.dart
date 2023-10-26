06d5fa509ab45652e6b92b870011b39abe60ebef
Mario E. Bermonti-Pérez <mbermonti1132@gmail.com>
2023-10-26 10:53:30 -0400

    refactor(stim): Improve name of prepareStim

--- lib/src/stim/stim_view.dart
+++ lib/src/stim/stim_view.dart
@@ -14,7 +14,7 @@ class StimView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    _stimController.prepareStim();
+    _stimController.prepareNextStim();
     _stimController.presentStim();
     return WillPopScope(
       onWillPop: () async {
