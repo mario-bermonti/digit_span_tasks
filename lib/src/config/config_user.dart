89c4ccc8dadb47e33b95e5b5599bcccf179b9837
Mario E. Bermonti-Pérez <mbermonti1132@gmail.com>
2023-11-01 13:33:55 -0400

    improv: Simplify user config
    Remove attributes that should not be accessible to user and make it a
    simple data model.

--- lib/src/config/config_user.dart
+++ lib/src/config/config_user.dart
@@ -1,12 +1,9 @@
 import 'package:get/get_state_manager/get_state_manager.dart';
-import 'package:mdigits/src/randomize.dart';
 
 class ConfigUser extends GetxController {
   /// List of stim to present to the user
   late final List<String> stimListPractice;
   late final List<String> stimListExperimental;
-
-  bool isPractice = true;
 
   /// Unique id to identify all the participant's data
   final String participantID;
@@ -14,8 +11,5 @@ class ConfigUser extends GetxController {
   ConfigUser(
       {required stimListPractice,
       required stimListExperimental,
-      required this.participantID}) {
-    this.stimListPractice = randomizeDigitsInSets(stimListPractice);
-    this.stimListExperimental = randomizeDigitsInSets(stimListExperimental);
-  }
+      required this.participantID});
 }
