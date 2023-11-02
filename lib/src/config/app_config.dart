777dcbe27da64ea8e96fabde40e641c25a8c37f1
Mario E. Bermonti-Pérez <mbermonti1132@gmail.com>
2023-11-02 09:57:12 -0400

    fix: Randomize digit set in user config
    Can't be done in app config because user config fields are final.

--- lib/src/config/app_config.dart
+++ lib/src/config/app_config.dart
@@ -1,15 +1,9 @@
 import 'package:get/get.dart';
 import 'package:mdigits/src/config/user_config.dart';
-import 'package:mdigits/src/randomize.dart';
 
 class AppConfig extends GetxController {
   final UserConfig userConfig;
   bool isPractice = true;
 
-  AppConfig({required this.userConfig}) {
-    userConfig.stimListPractice =
-        randomizeDigitsInSets(userConfig.stimListPractice);
-    userConfig.stimListExperimental =
-        randomizeDigitsInSets(userConfig.stimListExperimental);
-  }
+  AppConfig({required this.userConfig});
 }
