7b310f0c0b789969a699c8827fbceb9adf61385b
Mario E. Bermonti-Pérez <mbermonti1132@gmail.com>
2023-11-02 09:46:46 -0400

    refactor: Make user config a field in app config

--- lib/src/config/app_config.dart
+++ lib/src/config/app_config.dart
@@ -3,21 +3,13 @@ import 'package:mdigits/src/config/user_config.dart';
 import 'package:mdigits/src/randomize.dart';
 
 class AppConfig extends GetxController {
-  /// List of stim to present to the user
-  late final List<String> stimListPractice;
-  late final List<String> stimListExperimental;
+  final UserConfig userConfig;
+  bool isPractice = true;
 
-  late bool isPractice = true;
-
-  /// Unique id to identify all the participant's data
-  late final String participantID;
-
-  AppConfig({
-    required UserConfig configUser,
-  }) {
-    stimListPractice = randomizeDigitsInSets(configUser.stimListPractice);
-    stimListExperimental =
-        randomizeDigitsInSets(configUser.stimListExperimental);
-    participantID = configUser.participantID;
+  AppConfig({required this.userConfig}) {
+    userConfig.stimListPractice =
+        randomizeDigitsInSets(userConfig.stimListPractice);
+    userConfig.stimListExperimental =
+        randomizeDigitsInSets(userConfig.stimListExperimental);
   }
 }
