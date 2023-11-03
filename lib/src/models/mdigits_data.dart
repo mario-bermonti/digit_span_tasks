ff403df456e7ad53e92205e28edb536c87e8c5bc
Mario E. Bermonti-Pérez <mbermonti1132@gmail.com>
2023-11-02 20:28:12 -0400

    doc: Update MDigitsData docs

--- lib/src/models/mdigits_data.dart
+++ lib/src/models/mdigits_data.dart
@@ -1,9 +1,12 @@
 import 'package:mdigits/src/models/mdigits_data_phase.dart';
 
-/// Structured data collected by MDigits
-/// Used to provide data nicely formatted to the user
+/// Structured data collected by MDigits.
+/// Used to provide data nicely formatted to the user.
 class MDigitsData {
+  /// Data collected during the practice phase.
   MDigitsDataPhase practiceData;
+
+  /// Data collected during the experimental phase.
   MDigitsDataPhase experimentalData;
 
   MDigitsData({required this.practiceData, required this.experimentalData});
