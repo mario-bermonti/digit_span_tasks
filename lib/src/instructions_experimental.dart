ff35b1e424b96beb887155d08fc8bbc7bdd26146
Mario E. Bermonti-Pérez <mbermonti1132@gmail.com>
2023-11-13 17:33:54 -0400

    refactor: Define instructions_experimental

---
+++ lib/src/instructions_experimental.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+import 'package:mdigits/src/components/instructions/general_instructions.dart';
+
+class InstructionsExperimental extends StatelessWidget {
+  const InstructionsExperimental({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return GeneralInstructions(
+      children: Column(
+        children: [
+          Text(
+            'Terminamos la práctica',
+            style: Theme.of(context).textTheme.titleLarge,
+          ),
+          const SizedBox(height: 15),
+          Text(
+            'Trabajemos en los ejercicios principales',
+            style: Theme.of(context).textTheme.titleLarge,
+          ),
+        ],
+      ),
+    );
+  }
+}
