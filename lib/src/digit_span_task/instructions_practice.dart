9c80e2ac6bcfc635e5d43d57ec0dba2a2b3530df
Mario E. Bermonti-Pérez <mbermonti1132@gmail.com>
2023-11-15 09:22:14 -0400

    refactor: Parametrize practice instructions

--- lib/src/digit_span_task/instructions_practice.dart
+++ lib/src/digit_span_task/instructions_practice.dart
@@ -1,8 +1,12 @@
+import 'package:digit_span_tasks/src/digit_span_task/components/instructions/instructions_model.dart';
 import 'package:flutter/material.dart';
 import 'package:digit_span_tasks/src/digit_span_task/components/instructions/general_instructions.dart';
+import 'package:get/get.dart';
 
 class InstructionsPractice extends StatelessWidget {
-  const InstructionsPractice({
+  final InstructionsModel _instructions = Get.find();
+
+  InstructionsPractice({
     Key? key,
   }) : super(key: key);
 
@@ -10,7 +14,7 @@ class InstructionsPractice extends StatelessWidget {
   Widget build(BuildContext context) {
     return GeneralInstructions(
       children: Text(
-        'Comenzaremos practicando',
+        _instructions.practice,
         style: Theme.of(context).textTheme.titleLarge,
       ),
     );
