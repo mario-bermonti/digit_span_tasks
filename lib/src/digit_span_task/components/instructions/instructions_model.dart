35fee5786a40f59270371a04dc9ce47450a202d1
Mario E. Bermonti-Pérez <mbermonti1132@gmail.com>
2023-11-19 12:52:08 -0400

    improve: Make instruction fields widgets

--- lib/src/digit_span_task/components/instructions/instructions_model.dart
+++ lib/src/digit_span_task/components/instructions/instructions_model.dart
@@ -1,15 +1,10 @@
+import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
 /// Model for specifying the instructions to be used in the task
 class InstructionsModel extends GetxController {
-  final String introduction;
-  final String practice;
-  final String experimental;
-  final String reminderInTask;
-
-  InstructionsModel(
-      {required this.introduction,
-      required this.practice,
-      required this.experimental,
-      required this.reminderInTask});
+  late Widget introduction;
+  late Widget practice;
+  late Widget experimental;
+  late Widget reminderInTask;
 }
