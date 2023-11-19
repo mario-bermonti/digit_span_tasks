de0baf26d0e58ad7b2354c7e52db9452aafd7051
Mario E. Bermonti-Pérez <mbermonti1132@gmail.com>
2023-11-19 13:39:49 -0400

    improve: Define the digit span forward

---
+++ lib/src/digit_span_forward.dart
@@ -0,0 +1,31 @@
+import 'package:digit_span_tasks/digit_span_tasks.dart';
+import 'package:digit_span_tasks/src/digit_span_task/components/config/app_config.dart';
+import 'package:digit_span_tasks/src/digit_span_task/components/instructions/instructions_model.dart';
+import 'package:digit_span_tasks/src/digit_span_task/digit_span_tasks_activity.dart';
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+class DigitSpanForward extends StatelessWidget {
+  late final AppConfig _config;
+
+  DigitSpanForward({super.key, required UserConfig config}) {
+    _config = Get.put(AppConfig(userConfig: config));
+    InstructionsModel instructions = Get.put(InstructionsModel());
+
+    instructions.introduction =
+        const Text('Recuerda los números en el orden en que los veas');
+    instructions.practice = const Text('Comenzaremos practicando');
+    instructions.experimental = Column(children: const [
+      Text('Terminamos la práctica'),
+      Text(
+          'Toma un breve descanso y trabajaremos en los ejercicios principales'),
+    ]);
+    instructions.reminderInTask =
+        const Text('Recuerda, escribe los números en el orden en que los veas');
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return DigitSpanTasksActivity();
+  }
+}
