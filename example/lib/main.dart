4346299997518768911a6e9479e813efe29cb5c3
Mario E. Bermonti-Pérez <mbermonti1132@gmail.com>
2023-11-21 11:36:30 -0400

    improve(example): Change color of buttons

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -12,9 +12,10 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return const GetMaterialApp(
+    return GetMaterialApp(
       title: 'DigitSpanTasks',
-      home: MyHomePage(),
+      home: const MyHomePage(),
+      theme: ThemeData(primarySwatch: Colors.grey),
     );
   }
 }
