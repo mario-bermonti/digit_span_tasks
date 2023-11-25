52ffc7351db477c213db5b187b6daf59797aa6c8
Mario E. Bermonti-Pérez <mbermonti1132@gmail.com>
2023-11-25 13:01:13 -0400

    doc: Add doc to data_model

--- lib/src/digit_span_task/components/data/data_model.dart
+++ lib/src/digit_span_task/components/data/data_model.dart
@@ -5,7 +5,10 @@ import 'package:digit_span_tasks/src/digit_span_task/components/data/trial_data.
 /// phase (practice or experimental). It was designed to be used internally
 /// by DigitSpanTask.
 class DataModel {
+  /// data about all the trials for the current session
   final List<TrialData> trialData = <TrialData>[];
+
+  /// data about the current session
   final SessionModel sessionData = SessionModel();
 
   @override
