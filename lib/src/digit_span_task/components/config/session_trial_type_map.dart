8ff6f080c17d6b89f502049eb33f48f6ef1ec9fc
Mario E. Bermonti-Pérez <mbermonti1132@gmail.com>
2024-02-27 08:49:07 -0400

    doc: Add docstring to session to trial type map

--- lib/src/digit_span_task/components/config/session_trial_type_map.dart
+++ lib/src/digit_span_task/components/config/session_trial_type_map.dart
@@ -2,6 +2,10 @@ import 'package:cognitive_data/models/trial_type.dart';
 import 'package:digit_span_tasks/src/digit_span_task/components/config/session_type.dart';
 import 'package:digit_span_tasks/src/digit_span_task/components/exceptions/sessiontype_conversion_exception.dart';
 
+/// Maps from a [SessionType] to the corresponding [TrialType].
+/// This mapping is essential because creating a [Trial] object requires a
+/// [TrialType], and this conversion provides a straightforward way to obtain
+/// the required [TrialType] based on the given [SessionType].
 const Map<SessionType, TrialType> sessionToTrialTypeMap = {
   SessionType.practice: TrialType.practice,
   SessionType.experimental: TrialType.experimental,
