f35ab0dabe22de189913a70dbe19b3cfcc5e6e69
Mario E. Bermonti-Pérez <mbermonti1132@gmail.com>
2024-02-27 08:32:09 -0400

    improve: Impl fun to convert session to trial type

--- lib/src/digit_span_task/components/config/session_trial_type_map.dart
+++ lib/src/digit_span_task/components/config/session_trial_type_map.dart
@@ -1,7 +1,16 @@
 import 'package:cognitive_data/models/trial_type.dart';
 import 'package:digit_span_tasks/src/digit_span_task/components/config/session_type.dart';
+import 'package:digit_span_tasks/src/digit_span_task/components/exceptions/sessiontype_conversion_exception.dart';
 
 const Map<SessionType, TrialType> sessionToTrialTypeMap = {
   SessionType.practice: TrialType.practice,
   SessionType.experimental: TrialType.experimental,
 };
+
+TrialType convertSessionToTrialType(SessionType sessionType) {
+  final TrialType? trialType = sessionToTrialTypeMap[sessionType];
+  if (trialType == null) {
+    throw DSTSessionTypeConversionException();
+  }
+  return trialType;
+}
