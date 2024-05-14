d9589ba000137ad4634156c425caaedc132cf998
Mario E. Bermonti-Pérez <mbermonti1132@gmail.com>
2024-05-14 15:30:31 -0400

    refactor: Remove ref to practice from instructions

--- lib/src/digit_span_task/components/instructions/instructions_model.dart
+++ lib/src/digit_span_task/components/instructions/instructions_model.dart
@@ -4,7 +4,6 @@ import 'package:get/get.dart';
 /// Model for specifying the instructions to be used in the task
 class InstructionsModel extends GetxController {
   late Widget introduction;
-  late Widget practice;
   late Widget experimental;
   late Widget reminderInTask;
 }
