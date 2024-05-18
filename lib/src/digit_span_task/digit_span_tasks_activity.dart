d068a795b220a1bb8abe3d08d078580a36ac319d
Mario E. Bermonti-Pérez <mbermonti1132@gmail.com>
2024-05-18 15:23:36 -0400

    improve: Remove integrated start screen

--- lib/src/digit_span_task/digit_span_tasks_activity.dart
+++ lib/src/digit_span_task/digit_span_tasks_activity.dart
@@ -1,48 +1,17 @@
-import 'package:digit_span_tasks/src/digit_span_task/components/config/session_type.dart';
 import 'package:digit_span_tasks/src/digit_span_task/components/data/digit_span_task_data.dart';
-import 'package:flutter/material.dart';
 import 'package:get/get.dart';
-import 'package:digit_span_tasks/src/digit_span_task/components/config/ds_config.dart';
 import 'package:digit_span_tasks/src/digit_span_task/components/data/data_manager.dart';
-import 'package:digit_span_tasks/src/digit_span_task/components/activity/activity_controller.dart';
 import 'package:digit_span_tasks/src/digit_span_task/components/activity/activity_view.dart';
-import 'package:digit_span_tasks/src/digit_span_task/components/ui_components/default_appbar.dart';
-import 'package:digit_span_tasks/src/digit_span_task/components/ui_components/screen.dart';
 
 /// Runs DigitSpanTasks activities.
 /// It returns the data from the session when DigitSpanTask
 /// finishes.
-class DigitSpanTasksActivity extends StatelessWidget {
-  final DataManager _data = Get.put(DataManager());
-  late final ActivityController _activityController;
-  late final DSConfig _config;
+class DigitSpanTasksActivity {
+  Future<DigitSpanTaskData> run() async {
+    final DataManager _dataManager = Get.put(DataManager());
+    await Get.to(() => ActivityView());
 
-  DigitSpanTasksActivity({super.key}) {
-    _config = Get.find();
-    _activityController = Get.put(ActivityController());
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Screen(
-      appBar: createAppBar(context: context),
-      children: Column(
-        children: <Widget>[
-          const SizedBox(height: 25),
-          ElevatedButton(
-            onPressed: () async {
-              await Get.to(() => ActivityView());
-
-              DigitSpanTaskData digitSpanTasksData = _data.export();
-              Get.back(result: digitSpanTasksData);
-            },
-            child: Text(
-              'Comenzar',
-              style: Theme.of(context).textTheme.titleLarge,
-            ),
-          ),
-        ],
-      ),
-    );
+    DigitSpanTaskData data = _dataManager.export();
+    return data;
   }
 }
