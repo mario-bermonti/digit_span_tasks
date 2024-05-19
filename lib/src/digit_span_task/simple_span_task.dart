0f88d9d5fb32f546cabad02efc905a4afb42304a
Mario E. Bermonti-Pérez <mbermonti1132@gmail.com>
2024-05-19 17:07:31 -0400

    doc: Update docstrings simple span task

--- lib/src/digit_span_task/simple_span_task.dart
+++ lib/src/digit_span_task/simple_span_task.dart
@@ -5,13 +5,14 @@ import 'package:digit_span_tasks/src/digit_span_task/components/activity/activit
 
 import 'components/config/ds_config.dart';
 
-/// Runs DigitSpanTasks activities.
-/// It returns the data from the session when DigitSpanTask
-/// finishes.
+/// Entry point for running a simple span task.
+/// It returns the data from the session when it finishes.
 class SimpleSpanTask {
   SimpleSpanTask({required config}) {
     final _config = Get.put(DSConfig(userConfig: config));
   }
+
+  /// Run a simple span task and return the data collected during the session.
   Future<SimpleSpanData> run() async {
     final DataManager dataManager = Get.put(DataManager());
     await Get.to(() => ActivityView());
